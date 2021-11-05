b9f4f21718ba4f3b189b5349a773137e457bd1d9
Ryan Oliver <ryannoliverr@gmail.com>
2021-11-05 10:14:30 -0400

    Changed summer page content to winter page content

--- src/config.js
+++ src/config.js
@@ -1,15 +1,17 @@
 let options = {
   title: "<strong>Select gift(s)</strong> <em>(select as many as you would like):</em>",
-  heroTitle: "<h1>Help a Lonely 'Backyard Dog' Survive the Summer</h1>",
+  heroTitle: "<h1>Help a Neglected Dog Survive the Winter</h1>",
   heroCopy: `
-    <p>The scorching sun and unrelenting heat of summer can be deadly for dogs chained outside without adequate shelter. Your much-needed gift today will help provide a lonely dog with support and care, or you can give shelter and some relief from the season's worst weather by sponsoring a sturdy doghouse.</p>
-    <p class='small-bold'>Your care package of any size will make a life-changing difference to a dog in need. <em>Dogs need you!</em></p>
+    <p>For dogs kept outside, the bone-chilling temperatures and bitter winds this winter could be deadly. <span style="font-weight: bold">They need your help right now.</span></p>
+    <p>Will you change a dog's life by sending an emergency care package today? PETA provides lonely, neglected dogs with shelter,
+    lightweight tie-outs to replace heavy chains, and even water buckets that we secure to prevent tipping. Your support will go
+    straight to helping dogs suffering in the harsh elements without adequate care and shelter.</p>
     <input type="button" value="Send your care package!" onClick="document.getElementById('en__component--heading').scrollIntoView();">
     `,
   responsiveTitle: "<strong>Select Gift(s):</strong>",
   customAmount: true,
   customAmountImage:
-    "https://acb0a5d73b67fccd4bbe-c2d8138f0ea10a18dd4c43ec3aa4240a.ssl.cf5.rackcdn.com/10003/Shop-Choose-Amount-SDH21.jpg",
+    "https://acb0a5d73b67fccd4bbe-c2d8138f0ea10a18dd4c43ec3aa4240a.ssl.cf5.rackcdn.com/10003/2021-Shop-Choose-Amount-WDH1.jpg?v=1636121242000",
   heroImage: "https://acb0a5d73b67fccd4bbe-c2d8138f0ea10a18dd4c43ec3aa4240a.ssl.cf5.rackcdn.com/10003/img-doghouse.jpg?v=1635978213000",
   heroImageResponsive:
     "https://acb0a5d73b67fccd4bbe-c2d8138f0ea10a18dd4c43ec3aa4240a.ssl.cf5.rackcdn.com/10003/img-doghouse.jpg?v=1635978213000",
